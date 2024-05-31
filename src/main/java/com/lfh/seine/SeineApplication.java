93026ee631acdd0bf34f5b8e6a46f09143a60483
lfh <920298316@qq.com>
2024-05-31 17:14:22 +0800

    feat(): 去掉多余配置

--- src/main/java/com/lfh/seine/SeineApplication.java
+++ src/main/java/com/lfh/seine/SeineApplication.java
@@ -1,11 +1,9 @@
 package com.lfh.seine;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.lfh.seine.web.user.mapper")
 public class SeineApplication {
 
     public static void main(String[] args) {
