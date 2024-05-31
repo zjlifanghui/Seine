507d1392bf41ba1f5503134d965190d516a2306e
lfh <920298316@qq.com>
2024-05-31 14:58:29 +0800

    feat(): 初始话项目

---
+++ src/main/java/com/lfh/seine/SeineApplication.java
@@ -0,0 +1,13 @@
+package com.lfh.seine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lfh/seine/SeineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lfh.seine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
